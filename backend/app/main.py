a2aa79704a88583230075d4899509f5459f47aad
Liza <kuznecovaElizaveta2006@yandex.ru>
2026-01-27 18:37:32 +0300

    backend -> docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,9 @@
-import os
 from pydoc import text
 
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 # from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 
 from app.database import Base, engine, get_db
@@ -35,24 +33,6 @@ app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(email.router, prefix="/email", tags=["email"])
 app.include_router(tasks.router, prefix="/tasks", tags=["task"])
 app.include_router(ai.router, prefix="/ai", tags=["ai"])
-
-
-frontend_dir = os.path.join(os.path.dirname(__file__), "..", "..", "frontend")
-app.mount("/static", StaticFiles(directory=frontend_dir), name="static")
-
-
-# @app.get("/{full_path:path}")
-# async def serve_frontend(full_path: str):
-#     if (
-#         full_path.startswith("api/")
-#         or full_path.startswith("docs")
-#         or full_path.startswith("openapi")
-#     ):
-#         return {"detail": "Not Found"}, 404
-#     file_path = os.path.join(frontend_dir, "index.html")
-#     if os.path.exists(file_path):
-#         return FileResponse(file_path)
-#     return {"detail": "Frontend not found"}, 500
 
 
 @app.get("/db-test")
